c1ac8b4a639632d5b52db41f4dfd1ac00d482383
jesQM <jezuquema@gmail.com>
2020-10-24 17:02:35 +0200

    Fix: Added require component to Selected Renderer

--- Assets/Scripts/CelestialBodies/CBSelectedRenderer.cs
+++ Assets/Scripts/CelestialBodies/CBSelectedRenderer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CelestialBody))]
 public class CBSelectedRenderer : MonoBehaviour
 {
     private CelestialBody planet;
