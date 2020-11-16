9797e6475f91f7c8e490a4856952d7611ce5ba80
jesQM <jezuquema@gmail.com>
2020-11-17 00:20:29 +0100

    Implemented some UI
    Population display, Pause and Speed Change

---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    public TextMeshProUGUI PopulationTitle;
+    public TextMeshProUGUI PopulationCount;
+
+    private Team TeamOfDisplay;
+    
+    void Start()
+    {
+        this.TeamOfDisplay = GameManager.Instance.HumanPlayer;
+
+        Color colour = TeamOfDisplay.Colour;
+
+        PopulationTitle.outlineColor = colour;
+        PopulationTitle.fontMaterial.SetColor(ShaderUtilities.ID_UnderlayColor, colour);
+
+        PopulationCount.outlineColor = colour;
+        PopulationCount.fontMaterial.SetColor(ShaderUtilities.ID_UnderlayColor, colour);
+    }
+    
+    void Update()
+    {
+        string populationCountText = TeamOfDisplay.CurrentTroopCount + "/" + TeamOfDisplay.MaxTroopCount;
+        PopulationCount.text = populationCountText;
+    }
+
+    public void PauseGame()
+    {
+        if (Time.timeScale == 0)
+            Time.timeScale = 1;
+        else
+            Time.timeScale = 0;
+    }
+
+    public void TimeScale(float amount)
+    {
+        Time.timeScale = amount;
+    }
+
+}
