6174df36adce9799cd7caec90dd1883cb9fb52af
jesQM <jezuquema@gmail.com>
2020-11-18 00:59:29 +0100

    Main Menu created

---
+++ Assets/Scripts/Util/SkyboxRotator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxRotator : MonoBehaviour
+{
+    public float rotationSpeed;
+    public float xSpeed;
+    public float ySpeed;
+    public float zSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //RenderSettings.skybox.SetFloat("_Rotation", Time.time * rotationSpeed);
+        Skybox s = GetComponent<Skybox>();
+        s.transform.rotation = Quaternion.Euler(Time.time * rotationSpeed * xSpeed, Time.time * rotationSpeed * ySpeed, Time.time * rotationSpeed * zSpeed);
+    }
+}
