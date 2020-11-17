133e20717c9dbc77a68b0168952ba8cb6af0ab94
jesQM <jezuquema@gmail.com>
2020-11-17 20:28:35 +0100

    Implemented pause menu

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -1,14 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
+using System;
 
 public class UIManager : MonoBehaviour
 {
+    [Header("Population")]
     public TextMeshProUGUI PopulationTitle;
     public TextMeshProUGUI PopulationCount;
+    
+    [Header("Pause Menu")]
+    public GameObject PauseMenu;
+    public Image PauseMenuBackground;
+
+
 
     private Team TeamOfDisplay;
+    private float TimeScaleBeforePause = 1;
     
     void Start()
     {
@@ -21,6 +31,8 @@ public class UIManager : MonoBehaviour
 
         PopulationCount.outlineColor = colour;
         PopulationCount.fontMaterial.SetColor(ShaderUtilities.ID_UnderlayColor, colour);
+
+        PauseMenu.SetActive(false);
     }
     
     void Update()
@@ -29,17 +41,49 @@ public class UIManager : MonoBehaviour
         PopulationCount.text = populationCountText;
     }
 
-    public void PauseGame()
+    public void PauseBtnClick()
     {
-        if (Time.timeScale == 0)
-            Time.timeScale = 1;
-        else
-            Time.timeScale = 0;
+        TimeScaleBeforePause = Time.timeScale;
+        Time.timeScale = 0;
+        ShowPauseMenu();
     }
 
-    public void TimeScale(float amount)
+    public void TimeScaleBtnClick(float amount)
     {
         Time.timeScale = amount;
     }
 
+    public void Resume()
+    {
+        Time.timeScale = TimeScaleBeforePause;
+        HidePauseMenu();
+    }
+
+    private void ShowPauseMenu()
+    {
+        PauseMenu.SetActive(true);
+        StartCoroutine(ImageChangeOpacity(PauseMenuBackground, 0.5f));
+    }
+
+    private void HidePauseMenu()
+    {
+        PauseMenu.SetActive(false);
+        StartCoroutine(ImageChangeOpacity(PauseMenuBackground, 0));
+    }
+
+    IEnumerator ImageChangeOpacity(Image image, float newOpacity) {
+        float time = 0.2f;
+        float percent = 0;
+
+        Color currentColor = image.color;
+        Color desiredColor = new Color(currentColor.r, currentColor.g, currentColor.b, newOpacity);
+
+        float t = Time.unscaledTime;
+        while (percent <= 1)
+        {
+            percent = percent + Time.unscaledDeltaTime * 1/time;
+            image.color = Color.Lerp(currentColor, desiredColor, percent);
+            yield return null;
+        }
+    }
 }
