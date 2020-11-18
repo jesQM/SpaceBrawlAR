d12d7b0c8911db11e9fc285562f0fa3a695d175d
jesQM <jezuquema@gmail.com>
2020-11-18 13:32:44 +0100

    Created Level Chooser and Scene Change

---
+++ Assets/Scripts/Util/LevelLoader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    private static float TransitionTime;
+
+    public void LoadLevel(string sceneName) {
+        StartCoroutine(LoadScene(sceneName, TransitionTime));
+    }
+
+    IEnumerator LoadScene(string name, float transitionTime)
+    {
+        SceneManager.LoadScene(name);
+        yield return null;
+    }
+}
