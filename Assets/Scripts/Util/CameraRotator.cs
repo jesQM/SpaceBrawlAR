a81afd6e4c1fe7f1ae2dfeeba7122c69feb5cb6a
jesQM <jezuquema@gmail.com>
2020-11-18 13:10:04 +0100

    Changed Skybox rotator to Camera Rotator

---
+++ Assets/Scripts/Util/CameraRotator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CameraRotator : MonoBehaviour
+{
+    public float rotationSpeed;
+    public float xSpeed;
+    public float ySpeed;
+    public float zSpeed;
+
+    Camera c;
+
+    void Start()
+    {
+        c = GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        c.transform.rotation = Quaternion.Euler(Time.time * rotationSpeed * xSpeed, Time.time * rotationSpeed * ySpeed, Time.time * rotationSpeed * zSpeed);
+    }
+}
