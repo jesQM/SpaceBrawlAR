8299bebc31476160e7647619562cc7e1c0742e7e
jesQM <jezuquema@gmail.com>
2020-11-24 00:48:53 +0100

    When only one team is alive the game ends

---
+++ Assets/Scripts/LevelCompleteChecker.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelCompleteChecker : MonoBehaviour
+{
+    public Action OnLevelEnd;
+    public Team Winner;
+    
+    void Update()
+    {
+        Team winner = null;
+        foreach(Team t in GameManager.Instance.AllTeams)
+        {
+            if (t.CurrentTroopCount > 0)
+            {
+                if (winner != null)
+                    return;
+                winner = t;
+            }
+        }
+
+        // If we are here, only one team had more than 0 ppl
+        Winner = winner;
+        LevelFinished();
+    }
+
+    private void LevelFinished()
+    {
+        OnLevelEnd?.Invoke();
+        FindObjectOfType<LevelLoader>().LoadLevel("MainMenu");
+    }
+}
