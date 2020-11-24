2492d6b51f228c14d84c19d9e3069e57d2c742ae
jesQM <jezuquema@gmail.com>
2020-11-24 16:46:27 +0100

    Implemented button click sound

---
+++ Assets/Scripts/UI/AudioManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioManager : MonoBehaviour
+{
+    public AudioClip BtnContinue;
+    public AudioClip BtnBack;
+
+    private AudioSource source;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        source.volume = 0.5f;
+    }
+
+    public void PlayBtnContinue()
+    {
+        source.clip = BtnContinue;
+        source.Play();
+    }
+
+    public void PlayBtnBack()
+    {
+        source.clip = BtnBack;
+        source.Play();
+    }
+}
