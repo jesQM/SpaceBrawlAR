5656cc39bb9b48f5998455b75ae6beabdbe60cd8
jesQM <jezuquema@gmail.com>
2020-11-26 19:10:46 +0100

    Implemented a BGM Player

--- Assets/Scripts/UI/AudioManager.cs
+++ Assets/Scripts/UI/AudioManager.cs
@@ -5,30 +5,14 @@ using UnityEngine;
 [RequireComponent(typeof(AudioSource))]
 public class AudioManager : MonoBehaviour
 {
-    public AudioClip BtnContinue;
-    public AudioClip BtnBack;
-
     private AudioSource source;
-
-    // Start is called before the first frame update
+    
     void Start()
     {
         source = GetComponent<AudioSource>();
         source.volume = 0.5f;
     }
-
-    public void PlayBtnContinue()
-    {
-        source.clip = BtnContinue;
-        source.Play();
-    }
-
-    public void PlayBtnBack()
-    {
-        source.clip = BtnBack;
-        source.Play();
-    }
-
+    
     public void PlaySound(AudioClip sound)
     {
         source.clip = sound;
---
+++ Assets/Scripts/UI/BackgroundMusicManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class BackgroundMusicManager : MonoBehaviour
+{
+    public AudioClip music;
+    private AudioSource source;
+
+    void Awake()
+    {
+        // Singleton
+        BackgroundMusicManager[] objs = GameObject.FindObjectsOfType<BackgroundMusicManager>();
+
+        if (objs.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+    
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        source.volume = 0.5f;
+
+        source.clip = music;
+        source.loop = true;
+        source.Play();
+    }
+}
